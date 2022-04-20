51f607e08733565ee8516aa611b367ddf7ed76bb
robin chen <aiflyme@gmail.com>
2022-04-20 18:03:19 +0800

    用户注册页面

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ Route::get('/','StaticPagesController@home')->name('home');
 Route::get('/help','StaticPagesController@help')->name('help');
 Route::get('/about','StaticPagesController@about')->name('about');
 
+Route::get('signup', 'UserController@create')->name('signup');
 /*
 Route::get('/', function () {
     return view('welcome');
